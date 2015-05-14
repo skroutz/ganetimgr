a3fb36552e38f5951b5928e414ea48bf6d7d217d
Dimitris Bliablias <dblia@skroutz.gr>
2015-05-14 11:59:11 +0300

    Create the new migration files for Django v1.7
    This patch creates the new migration files for Django versions > 1.7.

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('apply', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_login', models.BooleanField(default=True)),
+                ('force_logout_date', models.DateTimeField(null=True, blank=True)),
+                ('telephone', models.CharField(max_length=13, null=True, blank=True)),
+                ('organization', models.ForeignKey(blank=True, to='apply.Organization', null=True)),
+                ('user', models.OneToOneField(related_name='profile', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
