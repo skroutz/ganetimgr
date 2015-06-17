7816977f7c624d8c14997b15ad087abeddefc6b7
Dimitris Bliablias <dblia@skroutz.gr>
2015-06-17 11:28:20 +0300

    Add a verbose_name_plural attr for AuditEntry objects

--- auditlog/models.py
+++ auditlog/models.py
@@ -17,6 +17,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
 class AuditEntry(models.Model):
@@ -28,6 +29,9 @@ class AuditEntry(models.Model):
     job_id = models.IntegerField(null=True, blank=True)
     recorded = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = _("audit entries")
 
     def __unicode__(self):
         return "%s %s %s" % (self.requester, self.action, self.instance)
